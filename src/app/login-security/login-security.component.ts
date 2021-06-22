f6917f6f93b21d05f99807a8ce6b7fcc1ec12452
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2021-06-22 08:01:38 -0400

    password updates

--- src/app/login-security/login-security.component.ts
+++ src/app/login-security/login-security.component.ts
@@ -15,21 +15,6 @@ export class LoginSecurityComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.tokenService.getUser();
   }
-  toggleVisibility(event, id) {
-    const element = event.currentTarget;
-    const icon = element.querySelector('i');
-    const input = document.getElementById(id);
-    if (icon.classList.contains('fa-eye-slash')) {
-      icon.classList.remove('fa-eye-slash')
-      icon.classList.add('fa-eye')
-      input.setAttribute('type', 'text')
-    } else {
-      icon.classList.add('fa-eye-slash')
-      icon.classList.remove('fa-eye')
-      input.setAttribute('type', 'password')
-    }
-  }
-
 
   toggleVisibility(event, id) {
     const element = event.currentTarget;
