dcb1a9b4bd28c345dea4f759fb73da0f988e27d2
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2021-07-06 07:59:34 -0400

    my profile text

--- src/app/personal-details/personal-details.component.ts
+++ src/app/personal-details/personal-details.component.ts
@@ -37,6 +37,8 @@ export class PersonalDetailsComponent implements OnInit {
     if (this.helpers.isBrowser()) {
       this.user = this.tokenService.getUser();
       this.profile = this.user.profile;
+      this.helpers.setHeader();
+
       if (this.profile) {
         this.setFormAttributes();
       }
