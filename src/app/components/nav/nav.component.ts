b167de7b4039e1edc4bac952f031e874663660ac
David <david.inabinette@gmail.com>
2021-07-13 13:32:59 -0500

    nav icon larger;nav-blur added;

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -20,6 +20,21 @@ export class NavComponent implements OnInit, OnChanges {
       this.user = this.helpers.currentUser()
     }
   }
+  @HostListener('window:scroll') onScroll(e: Event): void {
+    const nav = this.helpers.doc().querySelector('nav');
+    if (this.helpers.isBrowser()) {
+      if (window.pageYOffset > 0) {
+      nav.style.background = 
+      'transparent linear-gradient(180deg, #1a2030 0%, #1a203000 100%) 0% 0% no-repeat padding-box';
+      nav.classList.add('nav-blur');
+      } else {
+        nav.classList.remove('scrolled');
+        nav.classList.remove('nav-blur');
+        nav.style.background =
+          'linear-gradient(180deg, #000000 -20%, #00000010 72%, #00000000 101%)';
+      }
+    }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.path = changes.route.currentValue;
